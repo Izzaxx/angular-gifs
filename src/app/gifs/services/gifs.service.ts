8c34cac27d41a0c9b7650b22f40483e8184c8cfe
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2022-10-18 14:49:58 -0700

    [UPDATE] Search bar

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _history: string[] = [];
+
+  get history() {
+    return [...this._history]
+  }
+
+  public searchGifs( query: string ) {
+    this._history.unshift( query );
+    console.log( this._history );
+  }
+
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent implements OnInit{
+
+  get history() {
+    return this.gifsService.history;
+  }
+
+  constructor( private gifsService: GifsService) {}
+
+  ngOnInit(): void {}
+
+}
